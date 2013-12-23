a71214a26d574c55bd08a4b51c0346c922d2a6e3
Filip Ljubicic <filip@filipljubicic.com>
2013-12-23 21:24:06 +0000

    Add Pricing and Style changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rowingdiary::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "pricing" => 'pages#pricing'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
