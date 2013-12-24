74bc3e44939c7864b0814348374fcd1b6122e73b
Filip Ljubicic <filip@filipljubicic.com>
2013-12-24 16:05:19 +0000

    Show.html.erb edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+#def create
+#  @user = User.create( user_params )
+#end
+
+#private
+
+# Use strong_parameters for attribute whitelisting
+# Be sure to update your create() and update() controller methods.
+
+#def user_params
+ # params.require(:user).permit(:avatar)
+#end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :ergos
+
+  #attr_accessible :avatar
+  
+  #has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
 end
