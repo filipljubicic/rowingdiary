ec971b6c58d510179722c7dd144bacdb0fdf5793
Filip Ljubicic <filip@filipljubicic.com>
2013-12-29 12:20:15 +0000

    Stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,4 @@
-raclass ChargesController < ApplicationController
+class ChargesController < ApplicationController
 	before_filter :authenticate_user!
 
 def new
@@ -14,21 +14,21 @@ def create
 	    
   )
 
-	#Stripe::Plan.create(
-	#  :amount => 999,
-	#  :interval => 'year',
-	#  :name => 'Individual',
-	#  :currency => 'usd',
-	#  :id => '1972'
-	#)
+	Stripe::Plan.create(
+	  :amount => 999,
+	  :interval => 'year',
+	  :name => 'Individual',
+	  :currency => 'usd',
+	  :id => '1972'
+	)
 
-	#Stripe::Coupon.create(
-  	#	:percent_off => 100,
-  	#	:duration => 'once',
-  	#	:id => '1988'
-#)
+	Stripe::Coupon.create(
+  		:percent_off => 100,
+  		:duration => 'once',
+  		:id => '1988'
+)
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
-  redirect_to charges_path notice: "Not authorized to edit this pin" if @card.nil?
+  redirect_to charges_path
 end
